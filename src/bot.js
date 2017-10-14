e58d2000e7925130df588ac5719187a065482f9c
Manzoni Vincent <vmanzoni.pro@gmail.com>
2017-10-14 14:28:23 +0200

    Create command database

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,18 @@
 const BotSettings = require ('../botSettings.json');
 const Console = require('console');
 const DiscordJS = require('discord.js-commando');
+const SQLite = require('sqlite');
 
 const CLIENT = new DiscordJS.Client({
 	owner: BotSettings.owner,
 	commandPrefix: '' //Disable prefix and use only mentions.
 });
 
-CLIENT.on('ready', () => {
-	Console.info('Ready.');
-});
+CLIENT.on('ready', () => { Console.info('Ready.'); });
+
+CLIENT.setProvider(SQLite.open('./commands.sqlite3').then(
+	(db) => new DiscordJS.SQLiteProvider(db),
+	(err) => Console.error(err))
+);
 
 CLIENT.login(BotSettings.token);
