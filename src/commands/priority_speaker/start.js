b76216b08bd5498031c2e4a2839f8c08ff290237
Manzoni Vincent <vmanzoni.pro@gmail.com>
2018-02-24 15:42:09 +0100

    Update start command

--- src/commands/priority_speaker/start.js
+++ src/commands/priority_speaker/start.js
@@ -23,7 +23,7 @@ module.exports = class StartCommand extends DiscordJS.Command {
 	hasPermission(msg) {
 		const havePermission = msg.member.roles.has(BotSettings.administrationRole) || msg.member.id === BotSettings.owner;
 
-		if(!havePermission) {
+		if (!havePermission) {
 			Console.log('<' + msg.member.displayName + '> tried to execute the "' + this.name + '" command, but he didn\'t have the permission to do this.');
 		}
 
@@ -33,10 +33,10 @@ module.exports = class StartCommand extends DiscordJS.Command {
 	speakingCallback(user, speaking) {
 		const CLIENT = user.client;
 
-		if(CLIENT.user.presence.status === 'online' && CLIENT.prioritySpeakers.has(user.id)) {
+		if (CLIENT.user.presence.status === 'online' && CLIENT.prioritySpeakers.has(user.id)) {
 			const guildMember = CLIENT.guilds.get(BotSettings.guild).member(user);
 
-			if(speaking) {
+			if (speaking) {
 				Console.log('<' + guildMember.displayName + '> is talking. Mute those who arn\'t in the priority speakers list and in the same channel.');
 
 				guildMember.voiceChannel.members.map((member) => {
@@ -56,34 +56,28 @@ module.exports = class StartCommand extends DiscordJS.Command {
 	async run(msg, ) {
 		const VOICE_CHANNEL = msg.member.voiceChannel;
 
-		if(VOICE_CHANNEL) {
-			if(Array.from(BotSettings.notPrioritizedChannel).indexOf(VOICE_CHANNEL.id) === -1) {
+		if (VOICE_CHANNEL) {
+			if (Array.from(BotSettings.notPrioritizedChannel).indexOf(VOICE_CHANNEL.id) === -1) {
 				msg.member.voiceChannel
 					.join()
-					.then(
-						(connection) => {
-							connection.on('speaking', this.speakingCallback);
-							msg.client.user.setStatus('online')
-								.then(
-									() => {
-										msg.reply('Priority speaker enabled.')
-											.then(
-												() => Console.log('<' + msg.member.displayName + '> enabled the priority speaker bot.')
-											).catch(Console.error);
-									}
-								).catch(Console.error);
-						}
+					.then((connection) => msg.client.user.setStatus('online')
+						.then(() => msg.reply('Priority speaker enabled.')
+							.then(() => {
+								connection.on('speaking', this.speakingCallback);
+								Console.log('<' + msg.member.displayName + '> enabled the priority speaker bot.');
+							}).catch(Console.error)
+						).catch(Console.error)
 					).catch(Console.error);
 			} else {
 				msg.reply('Priority speaker can\'t be enabled because you\'re voice channel is in the non prioritize channels list.')
 					.then(
-						() => Console.log('<' + msg.member.displayName + '> tried to enable the priority speaker bot but the voice channel is in the non prioritize channels list.')
+					() => Console.log('<' + msg.member.displayName + '> tried to enable the priority speaker bot but the voice channel is in the non prioritize channels list.')
 					).catch(Console.error);
 			}
 		} else {
 			msg.reply('Priority speaker can\'t be enabled because you\'re not in a voice channel.')
 				.then(
-					() => Console.log('<' + msg.member.displayName + '> tried to enable the priority speaker bot but he wasn\'t in a voice channel.')
+				() => Console.log('<' + msg.member.displayName + '> tried to enable the priority speaker bot but he wasn\'t in a voice channel.')
 				).catch(Console.error);
 		}
 	}
